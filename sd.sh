574059347c48e8654b8f68a732825d453d076126
alvis1595 <39422675+alvis1595@users.noreply.github.com>
2025-07-14 01:24:31 -0500

    Create sd.sh

---
+++ sd.sh
@@ -0,0 +1,3 @@
+https://access.redhat.com/mt/es/articles/7127544
+https://docs.redhat.com/en/documentation/red_hat_ansible_automation_platform/2.5/html/ansible_automation_platform_migration/migration-artifact
+https://access.redhat.com/support/policy/updates/ansible-automation-platform#dates
